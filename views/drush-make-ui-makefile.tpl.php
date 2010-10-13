b38327119c17bc37e2c370060a157b83694a79d8
Eugen Mayer <eugen.mayer@kontextwork.de>
2010-10-13 20:02:57 +0200

    fixing up comments, copyrights etc

--- views/drush-make-ui-makefile.tpl.php
+++ views/drush-make-ui-makefile.tpl.php
@@ -1,19 +1,8 @@
 <?php
-// $Id: views-bonus-export-xml.tpl.php,v 1.2 2009/06/24 17:27:53 neclimdul Exp $
-/**
- * @file views-view-table.tpl.php
- * Template to display a view as a table.
- *
- * - $title : The title of this group of rows.  May be empty.
- * - $rows: An array of row items. Each row is an array of content
- *   keyed by field ID.
- * - $header: an array of headers(labels) for fields.
- * - $themed_rows: a array of rows with themed fields.
- * @ingroup views_templates
- */
+// $Id$
 
-?>
-<?php
+// Copyright (c) 2010 KontextWork
+// Author: Eugen Mayer
 // globals so we can be aware then core / api is only printed once
 $core_printed = $api_printed = FALSE;
 foreach($view->style_plugin->rendered_fields as $row) {
