cacaf67e1b47f63d9a29d01983030ed0253604cd
em <eugen.mayer@kontextwork.de>
2012-02-05 11:37:59 +0100

    code styles

--- views/drush-make-ui-makefile.tpl.php
+++ views/drush-make-ui-makefile.tpl.php
@@ -5,17 +5,18 @@
 // globals so we can be aware then core / api is only printed once
 $core_printed = $api_printed = FALSE;
 $packages = array();
-foreach($view->style_plugin->rendered_fields as $row) {
+foreach ($view->style_plugin->rendered_fields as $row) {
   foreach ($row as $key => $field) {
     // check that core / api is always printed once, even if several make files are combined
-    if( ($key == 'field_core_version_value' && $core_printed != FALSE) 
-        || ($key == 'field_drush_make_api_value' && $api_printed) ) {
+    if (($key == 'field_core_version_value' && $core_printed != FALSE)
+      || ($key == 'field_drush_make_api_value' && $api_printed)
+    ) {
       continue;
-    } 
+    }
     else if ($key == 'field_core_version_value') {
       $core_printed = TRUE;
     }
-    else if($key == 'field_drush_make_api_value') {
+    else if ($key == 'field_drush_make_api_value') {
       $api_printed = TRUE;
     }
     echo "$field\n";
